aed79d829ee9d5a34580ac4e0c0f75fb20c54cb0
Han Hong <han.hong.ky@gmail.com>
2018-02-20 09:44:07 -0600

    Update split_review_urls.py

--- Steam Crawler/scripts/split_review_urls.py
+++ Steam Crawler/scripts/split_review_urls.py
@@ -21,15 +21,15 @@ def parse_args():
 
     parser.add_argument(
         '--scraped-products',
-        help='Path to scraped products.jl file.',
+        help='$(pwd)/../Data-Crawler/output/products_all.jl'
     )
     parser.add_argument(
         '--output-dir',
-        help='Directory in which to place url part files.'
+        help='$(pwd)/../Data-Crawler/output'
     )
     parser.add_argument(
         '--pieces',
-        help='Number of URL files to produce.',
+	help='1',
         default=10
     )
     return parser.parse_args()
@@ -37,35 +37,16 @@ def parse_args():
 
 def main():
     args = parse_args()
-
-    with open(args.scraped_products) as f:
+    fx = open('url.txt','a')
+    
+    with open('products_all.jl') as f:
         rows = [json.loads(l) for l in f]
 
-    df = pd.DataFrame(rows)
+    for i in rows:
+	if 'user reviews' in i["sentiment"]
+		fx.write('\n' + i["reviews_url"])
 
-    # Filter out URLs for products that lack basic data or have no reviews.
-    blx_nontrivial = np.all(df[['id', 'reviews_url', 'title']].notnull(), axis=1)
-    blx_has_reviews = df['n_reviews'] > 0
-    blx = blx_nontrivial & blx_has_reviews
-
-    urls = df.loc[blx, 'reviews_url'].unique()
-    urls = shuffle(urls)
-
-    n = len(urls)
-    step = int(math.ceil(float(n)/args.pieces))
-
-    for n_part, bound in enumerate(range(0, n, step), start=1):
-        file_name = os.path.join(
-            args.output_dir,
-            'review_urls_{:02d}.txt'.format(n_part)
-        )
-
-        with open(file_name, 'w') as f:
-            piece = urls[bound:bound + step]
-            f.write('\n'.join(piece))
-
-    n_items = int(df.loc[blx, 'n_reviews'].sum())
-    print("There are <={0} reviews to be scraped.".format(n_items))
+    fx.close()
 
 if __name__ == "__main__":
     main()
